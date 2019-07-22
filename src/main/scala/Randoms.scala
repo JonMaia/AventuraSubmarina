c75b2e854704ed2e3e54cbe5f159fdeba1807288
miguelbada <miguelenriquebada07@gmail.com>
2019-07-22 18:58:25 -0300

    lanzar dado controlable

--- src/main/scala/Randoms.scala
+++ src/main/scala/Randoms.scala
@@ -3,9 +3,19 @@ import scala.util.Random
 
 object Randoms {
 
+  var numero: Int = -1
+
+  def lanzarControlable(valorControlable: Int): Unit = {
+    this.numero = valorControlable
+  }
+
+  def desactivarControlable(): Unit = {
+    this.numero = -1
+  }
+
   //Cuando se usa sin parametro es random
-  def lanzarDado(numero: Int = -1): Int = {
-    numero match {
+  def lanzarDado(): Int = {
+    this.numero match {
       case -1 => Random.nextInt(6)
       case n => n
      }
